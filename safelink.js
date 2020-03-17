ac3bffee88d42a4947dacfafd2ac08b951367171
kaizoku75 <decomboy@msn.com>
2020-03-17 16:54:47 +0100

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://yow-anime.blogspot.com/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.fansubclub.ga/p/download.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
